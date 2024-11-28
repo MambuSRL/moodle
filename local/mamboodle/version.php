1821d98723866d873de574d3c420d0df097f94d0
forfra <francesco.forghieri@mambu.it>
2024-11-28 14:50:03 +0100

    avanti

--- local/mamboodle/version.php
+++ local/mamboodle/version.php
@@ -2,6 +2,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024102801;
+$plugin->version   = 2024112801;
 $plugin->requires  = 2020061500;
 $plugin->component = 'local_mamboodle';
